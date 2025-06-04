bf44aae513523cc22f50bbd49fc45af129976c9f
Namestruky <fmangom292@gmail.com>
2025-06-04 11:53:47 +0200

    Realizacion de ejercicio, html clases y lógica

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let palos = ["♠", "♥", "♦", "♣"];
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let palo = palos[Math.floor(Math.random() * palos.length)];
+  let numero = numeros[Math.floor(Math.random() * numeros.length)];
+
+  let color = palo === "♥" || palo === "♦" ? "red" : "black";
+
+  let cardHeader = document.querySelector(".card-header");
+  let cardContent = document.querySelector(".card-content");
+  let cardFooter = document.querySelector(".card-footer");
+
+  cardHeader.innerHTML = palo;
+  cardHeader.style.color = color;
+  cardContent.innerHTML = numero;
+  cardContent.style.color = color;
+  cardFooter.innerHTML = palo;
+  cardFooter.style.color = color;
+
 };
